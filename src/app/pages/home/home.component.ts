18ee2254dff110e712e71b66688dc87b4394fab7
sebastianRavenna <sebastian.ravenna@gmail.com>
2024-12-29 19:34:29 -0300

    arreglo de video

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements AfterViewInit {
+  @ViewChild('videoPlayer') videoPlayer!: ElementRef<HTMLVideoElement>;
 
+  ngAfterViewInit() {
+    if (this.videoPlayer.nativeElement) {
+      this.videoPlayer.nativeElement.muted = true;
+      this.videoPlayer.nativeElement.play().catch(error => {
+        console.log("Error reproduciendo el video:", error);
+      });
+    }
+  }
 }
